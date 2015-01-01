27fab3a2a7f3927931e1f44b099438ef72731468
Niraj Pant <nirajpant7@gmail.com>
2015-01-01 10:46:13 -0600

    Tie prototype cell to delegate

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by Niraj  on 12/31/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -23,11 +23,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 5
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        
+        return cell
     }
     
     // UITableViewDelegate
